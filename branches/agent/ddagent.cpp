489dc6547bdc1477a260c6535f5c0b703c1fef21
hef@pbrfrat.com <hef@pbrfrat.com@a83e3a04-347e-183a-b928-8ae08996f7c8>
2011-10-30 04:36:57 +0000

    adding the default agent functions.

--- branches/agent/ddagent.cpp
+++ branches/agent/ddagent.cpp
@@ -1,5 +1,24 @@
 #include "ddagent.h"
+#include <QDebug>
 #include <jvmti.h>
+JNIEXPORT jint JNICALL
+Agent_OnLoad(JavaVM *vm, char *options, void *reserved)
+{
+    qDebug() << "DDAgent Loaded";
+    return 0;
+}
+JNIEXPORT jint JNICALL
+Agent_OnAttach(JavaVM *vm, char *options, void *reserved)
+{
+    qDebug() << "DDAgent Attached";
+    return 0;
+}
+JNIEXPORT void JNICALL
+JNI_OnUnload(JavaVM *vm, void *reserved)
+{
+    qDebug() << "DDAgent unLoaded";
+    return;
+}
 
 DDAgent::DDAgent()
 {
