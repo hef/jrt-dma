740b9c615a9440fa95c5dfd94b4c94cf8bf361eb
hef@pbrfrat.com <hef@pbrfrat.com@a83e3a04-347e-183a-b928-8ae08996f7c8>
2011-11-13 21:20:13 +0000

    Merginh in graph branch

---
+++ trunk/test/CS442Project.cpp
@@ -0,0 +1,84 @@
+#include "CS442Project.h"
+
+#ifndef TRUE
+#define TRUE 1
+#endif
+
+#ifndef FALSE
+#define FALSE 0
+#endif
+
+#ifndef CLEAR_GRAPH
+#define CLEAR_GRAPH TRUE
+#endif
+
+int main()
+{
+	graph<int> DAG;
+	list<int> outlist;
+
+	int vertexA = 1;
+	int vertexB = 2;
+	int vertexC = 3;
+	int vertexD = 4;
+	int vertexE = 5;
+
+	DAG.insertVertex(vertexA); // test case insert a vertex
+	DAG.insertVertex(vertexB); // test case insert a vertex
+	DAG.insertVertex(vertexC); // test case insert a vertex
+	DAG.insertVertex(vertexD); // test case insert a vertex
+	DAG.insertVertex(vertexE); // test case insert a vertex
+
+	DAG.insertEdge(vertexA,vertexC); // test case insert edge A to C
+	DAG.insertEdge(vertexA,vertexD); // test case insert edge A to D
+	DAG.insertEdge(vertexB,vertexA); // test case insert edge B to A
+	DAG.insertEdge(vertexB,vertexD); // test case insert edge B to D
+	DAG.insertEdge(vertexD,vertexE); // test case insert edge D to E
+	DAG.insertEdge(vertexE,vertexB); // test case insert edge E to B
+
+	if (DAG.empty()) // test case empty graph
+	{
+		cout << "no vertices so far\n";
+		//test case check num vertices
+		cout << "Number of vertices: " << DAG.numberOfVertices() << "\n";
+		//test case check num edges
+		cout << "Number of edges: " << DAG.numberOfEdges() << "\n";
+	}
+	else // test case non empty graph
+	{
+		cout << "Number of vertices: " << DAG.numberOfVertices() << "\n";
+		cout << "Number of edges: " << DAG.numberOfEdges() << "\n";
+	}
+
+	if (DAG.noCycle(DAG))
+		cout << "no cycle" << "\n";
+	else
+		cout << "has cycle" << "\n";
+
+	DAG.topologicalSort(DAG,outlist);
+
+	if (CLEAR_GRAPH)
+	{
+		DAG.clear();
+		cout << "Number of vertices: " << DAG.numberOfVertices() << "\n";
+		cout << "Number of edges: " << DAG.numberOfEdges() << "\n";
+	}
+/*
+	else
+	{
+		DAG.eraseEdge(vertexA,vertexB); // test case 
+		cout << "Number of vertices: " << DAG.numberOfVertices() << "\n";
+		cout << "Number of edges: " << DAG.numberOfEdges() << "\n";
+
+		DAG.eraseVertex(vertexB);
+		cout << "Number of vertices: " << DAG.numberOfVertices() << "\n";
+		cout << "Number of edges: " << DAG.numberOfEdges() << "\n";
+
+		DAG.eraseVertex(vertexA);
+		cout << "Number of vertices: " << DAG.numberOfVertices() << "\n";
+		cout << "Number of edges: " << DAG.numberOfEdges() << "\n";
+	}
+*/
+
+	return 0;
+}
---
+++ trunk/test/CS442Project.h
@@ -0,0 +1,8 @@
+#ifndef _CS442PROJECT_H
+#define _CS442PROJECT_H
+
+#pragma warning(disable:4786)
+
+#include "graph.h"
+
+#endif
